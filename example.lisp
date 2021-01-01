c877f050a66c1ce379b141a50d63584ef0898217
Eric Willisson <epw@google.com>
2021-01-01 17:24:28 -0500

    Change #! line to use /usr/bin/env

--- example.lisp
+++ example.lisp
@@ -1,4 +1,4 @@
-#! /usr/local/bin/exlisp
+#! /usr/bin/env exlisp
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
